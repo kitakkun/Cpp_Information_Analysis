e4e02956679cb7e8983b887f64c51327f75619cd
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-04 11:14:44 +0900

    NonLinearSolverクラスの定義

---
+++ NonLinearSolver.h
@@ -0,0 +1,14 @@
+#ifndef CPP_INFORMATION_ANALYSIS_NONLINEARSOLVER_H
+#define CPP_INFORMATION_ANALYSIS_NONLINEARSOLVER_H
+
+#include <functional>
+
+class NonLinearSolver {
+public:
+    static double secant(std::function<double(double)> f, double xInitial, double epsilon, std::function<void(int ,double)> onValueUpdate);
+    static double newton(std::function<double(double)> f, std::function<double(double)> fd, double xInitial, double epsilon, int maxStep, std::function<void(int, double)> onValueUpdate);
+    static double bisection(std::function<double(double)> f, double aInitial, double bInitial, double epsilon, std::function<void(int, double, double)> onValueUpdate);
+    static double regula_falsi(std::function<double(double)> f, double aInitial, double bInitial, double epsilon, std::function<void(int, double, double)> onValueUpdate);
+};
+
+#endif //CPP_INFORMATION_ANALYSIS_NONLINEARSOLVER_H
