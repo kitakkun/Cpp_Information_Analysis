c4b2106d6a318633762a430feeeebda748c13eac
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-04 11:17:45 +0900

    ニュートン法の実装

--- NonLinearSolver.cpp
+++ NonLinearSolver.cpp
@@ -7,7 +7,15 @@ double NonLinearSolver::secant(std::function<double(double)> f, double xInitial,
 
 double NonLinearSolver::newton(std::function<double(double)> f, std::function<double(double)> fd, double xInitial,
                                double epsilon, int maxStep, std::function<void(int, double)> onValueUpdate) {
-    return 0;
+    double x = xInitial;
+    int step = 0;
+    while(step < maxStep && std::abs(f(x)) > epsilon) {
+        double xNext = x - f(x) / fd(x);
+        onValueUpdate(step, xNext);
+        x = xNext;
+        step++;
+    }
+    return x;
 }
 
 double NonLinearSolver::bisection(std::function<double(double)> f, double aInitial, double bInitial, double epsilon,
